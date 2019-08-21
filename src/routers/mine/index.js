4c763acab81322d334e26ae557f1e1b042b44a52
msnfs <1939491952@qq.com>
2019-08-21 18:37:10 +0800

    aaa

---
+++ src/routers/mine/index.js
@@ -0,0 +1,4 @@
+export default{
+  path:'/mine',
+  components:()=> import('@/views/Mine')
+}
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="main">
+      <Header></Header>
+      <TabBar></TabBar>
+    </div>
+</template>
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';
+export default {
+   name:'Movie',
+   components:{
+     Header,
+     TabBar
+   }
+}
+</script>
+<style scoped>
+
+</style>
